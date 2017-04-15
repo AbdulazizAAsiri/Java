5758448037d48f5518e997bac351e258299cad4e
AbdulazizAAsiri <abdulaziz-asiri-@hotmail.com>
2017-04-14 20:44:53 -0700

    new version of binary search. The old one is buggy.
    The is a newer version because the older version was very buggy. Proofs will be included later.

--- binarysearch.java
+++ binarysearch.java
@@ -1,31 +1,35 @@
 public class binarysearch {
 	int search(int [] list , int target, int min , int max) {
-		if (min >= max) {
+		if (min > max) {
 			return -1; 
 		}
-		 int mid =(max+min) / 2 ; 
+		 int mid =((max+min) / 2) ; 
+
 		if (list[mid] == target) {
 			return target ; 
 		}
-		else if (list[mid] < target) {
-			return search(list, target, mid+1 , max); 
-
-		} else  {
+		else if (target< list[mid]) {
 			return search(list, target, min , mid-1); 
+		}
+		else  {
+			return search(list, target, mid+1 , max); 
 		}
 	}
 	 public static void main(String[] args) {
-		int list[] = {5,6,7,8,9,10,15} ; 
-		int target = 6; 
-		int max = list.length;
+		int [] list = {5,6,7,8,9,10,15} ; 
+		int target = 16; 
+		int max = list.length-1;
 		int min = 0 ; 
 		binarysearch objj = new binarysearch (); 
-		int value = objj.search(list,target,min,max);
+		// Testing the values, mathematical proofs wby induction ill be included later 
+		for (int i = 0 ; i<20 ; i++) {
+		int value = objj.search(list, i,min,max);
 		if (value  == target) {
 		System.out.println("Value found "+value);
 		}
 		else {
 			System.out.println("Could not find the value "+value);
 		}
+	}
 	}
 }
